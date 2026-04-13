d447b72fc946e71315e6535b3e86916c1ca3664f
Vlad <vlad.kustov07@gmail.com>
2026-04-13 13:50:49 +0200

    chasing works

--- Player/brammy/brammy.gd
+++ Player/brammy/brammy.gd
@@ -1,17 +1,18 @@
 extends CharacterBody3D
 
+enum State { IDLE, PATROL, CHASE, ATTACK }
+var state = State.IDLE
+
 @export var speed := 3.5
 @export var attack_range := 1.5
 @export var attack_damage := 10
 @export var health := 100
+@export var detection_radius := 30.0;
 
 var player: CharacterBody3D = null
 const GRAVITY = -9.8
 
 @onready var nav_agent := $NavigationAgent3D
-
-enum State { IDLE, PATROL, CHASE, ATTACK }
-var state = State.CHASE
 
 func _ready():
 	player = GlobalPlayer.player
@@ -19,16 +20,6 @@ func _ready():
 	await NavigationServer3D.map_changed
 	await get_tree().physics_frame
 	
-	#debugging
-	var map = nav_agent.get_navigation_map()
-	var closest_to_enemy = NavigationServer3D.map_get_closest_point(map, global_position)
-	var closest_to_player = NavigationServer3D.map_get_closest_point(map, player.global_position)
-
-	print("Enemy pos: ", global_position)
-	print("Closest navmesh point to enemy: ", closest_to_enemy)
-	print("Player pos: ", player.global_position)
-	print("Closest navmesh point to player: ", closest_to_player)
-	###
 	var timer = Timer.new()
 	add_child(timer)
 	timer.wait_time = 0.2
@@ -46,12 +37,7 @@ func _physics_process(delta: float) -> void:
 	if player == null:
 		return
 
-	var dist = global_position.distance_to(player.global_position)
-
-	if dist > attack_range:
-		state = State.CHASE
-	if dist <= attack_range:
-		state = State.ATTACK
+	_determineState()
 
 	match state:
 		State.IDLE:
@@ -59,24 +45,18 @@ func _physics_process(delta: float) -> void:
 		State.PATROL:
 			move_toward_player(3.0)
 		State.CHASE:
-			move_toward_player(5.0)
+			move_toward_player(4.0)
 		State.ATTACK:
 			attack()
-			move_and_slide()
 
 func attack():
 	pass
 
 func move_toward_player(move_speed: float):
-	if not nav_agent.is_target_reachable():
-		print("not reachable")
-		return
-		
 	if not nav_agent.is_navigation_finished():
 		var next_pos = nav_agent.get_next_path_position()
 		var direction = (next_pos - global_position)
 		direction = direction.normalized()
-		print(direction);
 		
 		velocity.x = direction.x * move_speed
 		velocity.z = direction.z * move_speed
@@ -87,3 +67,12 @@ func move_toward_player(move_speed: float):
 		velocity.z = 0
 	
 	move_and_slide()
+	
+func _determineState():
+	var dist = global_position.distance_to(player.global_position)
+
+	if dist < detection_radius:
+		state = State.CHASE
+	
+	if dist <= attack_range:
+		state = State.ATTACK
