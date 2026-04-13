5b9ce3e95d02d654ea543291b4b83f28093e4fd1
Vlad <vlad.kustov07@gmail.com>
2026-04-13 13:07:53 +0200

    bug

--- Player/brammy/brammy.gd
+++ Player/brammy/brammy.gd
@@ -1,64 +1,89 @@
 extends CharacterBody3D
 
-@export var speed := 3.5;
-@export var attack_range := 1.5;
-@export var attack_damage := 10;
-@export var health := 100;
+@export var speed := 3.5
+@export var attack_range := 1.5
+@export var attack_damage := 10
+@export var health := 100
 
-var player : CharacterBody3D = null;
-const Gravity = -9.8;
+var player: CharacterBody3D = null
+const GRAVITY = -9.8
 
-@onready var nav_agent := $NavigationAgent3D;
-enum State { IDLE, PATROL, CHASE, ATTACK };
-var state = State.PATROL;
+@onready var nav_agent := $NavigationAgent3D
+
+enum State { IDLE, PATROL, CHASE, ATTACK }
+var state = State.CHASE
 
 func _ready():
-	player = GlobalPlayer.player;
+	player = GlobalPlayer.player
+	
+	await NavigationServer3D.map_changed
+	await get_tree().physics_frame
+	
+	#debugging
+	var map = nav_agent.get_navigation_map()
+	var closest_to_enemy = NavigationServer3D.map_get_closest_point(map, global_position)
+	var closest_to_player = NavigationServer3D.map_get_closest_point(map, player.global_position)
+
+	print("Enemy pos: ", global_position)
+	print("Closest navmesh point to enemy: ", closest_to_enemy)
+	print("Player pos: ", player.global_position)
+	print("Closest navmesh point to player: ", closest_to_player)
+	###
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.2
+	timer.timeout.connect(_update_path)
+	timer.start()
 
 func _update_path():
 	if player:
-		nav_agent.target_position = player.global_position;
+		nav_agent.target_position = player.global_position
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity.y += Gravity * delta
+		velocity.y += GRAVITY * delta
 
 	if player == null:
 		return
 
-	if global_position.distance_to(player.global_position) < 10.0:
-		state = State.CHASE;
+	var dist = global_position.distance_to(player.global_position)
 
-	if global_position.distance_to(player.global_position) <= attack_range:
-		state = State.ATTACK;
+	if dist > attack_range:
+		state = State.CHASE
+	if dist <= attack_range:
+		state = State.ATTACK
 
-	match state: 
+	match state:
 		State.IDLE:
-			move_toward_player(0.0);
+			move_toward_player(0.0)
 		State.PATROL:
-			pass
+			move_toward_player(3.0)
 		State.CHASE:
-			move_toward_player(5.0);
+			move_toward_player(5.0)
 		State.ATTACK:
-			attack();
-
+			attack()
+			move_and_slide()
 
 func attack():
-	return
+	pass
 
-func move_toward_player(speed: float):
-	_update_path();
-	
+func move_toward_player(move_speed: float):
+	if not nav_agent.is_target_reachable():
+		print("not reachable")
+		return
+		
 	if not nav_agent.is_navigation_finished():
-		var next_pos = nav_agent.get_next_path_position();
-		var direction = (next_pos - global_position);
+		var next_pos = nav_agent.get_next_path_position()
+		var direction = (next_pos - global_position)
+		direction = direction.normalized()
+		print(direction);
 		
-		velocity.x = direction.x * speed;
-		velocity.y = direction.y * speed;
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
 		
-		look_at(Vector3(next_pos.x, global_position.y, next_pos.z));
+		look_at(Vector3(next_pos.x, global_position.y, next_pos.z))
 	else:
-		velocity.x = 0;
-		velocity.y = 0;
-		
-	move_and_slide();
+		velocity.x = 0
+		velocity.z = 0
+	
+	move_and_slide()
