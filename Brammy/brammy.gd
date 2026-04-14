ecde062b240e978f2c213a1940713877ada34e2a
Tim-olr <timolree1@gmail.com>
2026-04-14 14:40:05 +0200

    began start seq

--- Brammy/brammy.gd
+++ Brammy/brammy.gd
@@ -8,6 +8,8 @@ class_name enemy
 @export var PlayerArea: Area3D
 @export var interaction_timer: Timer
 @export var attention_area: Area3D
+@export var started: bool = false
+
 var can_open_door: bool = true
 var speed
 var target
@@ -30,6 +32,7 @@ func _ready() -> void:
 	target = possies.get_random_pos()
 	await get_tree().process_frame
 	PlayerCast.enabled = true
+
 func _process(delta: float) -> void:
 	for p in PlayerArea.get_overlapping_bodies():
 		if p.is_in_group("player"):
@@ -38,32 +41,33 @@ func _process(delta: float) -> void:
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if phase == 3:
-		speed = chaseSpeed
-		target = GlobalPlayer.player
-	else:
-		if PlayerCast.is_colliding() and PlayerCast.get_collider().is_in_group("player"):
-			has_collided = true
-			phase = 2
-			target = GlobalPlayer.player
-		elif can_check:
-			phase = 2
-			target = GlobalPlayer.player
-		if phase == 2:
+	if started:
+		if phase == 3:
 			speed = chaseSpeed
-		elif phase == 1:
-			speed = wanderingSpeed
-			if global_position.distance_to(target.global_position) < 3:
-				target = possies.get_random_pos()
-	if is_instance_valid(target):
-		nav.target_position = target.global_position if not target is Vector3 else target
-	var direction = (nav.get_next_path_position() - global_position).normalized()
-	velocity.x = lerp(velocity.x, direction.x * speed, accel * delta)
-	velocity.z = lerp(velocity.z, direction.z * speed, accel * delta)
-	var horizontal_velocity = Vector3(velocity.x, 0, velocity.z)
-	if horizontal_velocity.length() > 0.01:
-		look_at(global_position + horizontal_velocity, Vector3.UP)
-	move_and_slide()
+			target = GlobalPlayer.player
+		else:
+			if PlayerCast.is_colliding() and PlayerCast.get_collider().is_in_group("player"):
+				has_collided = true
+				phase = 2
+				target = GlobalPlayer.player
+			elif can_check:
+				phase = 2
+				target = GlobalPlayer.player
+			if phase == 2:
+				speed = chaseSpeed
+			elif phase == 1:
+				speed = wanderingSpeed
+				if global_position.distance_to(target.global_position) < 3:
+					target = possies.get_random_pos()
+		if is_instance_valid(target):
+			nav.target_position = target.global_position if not target is Vector3 else target
+		var direction = (nav.get_next_path_position() - global_position).normalized()
+		velocity.x = lerp(velocity.x, direction.x * speed, accel * delta)
+		velocity.z = lerp(velocity.z, direction.z * speed, accel * delta)
+		var horizontal_velocity = Vector3(velocity.x, 0, velocity.z)
+		if horizontal_velocity.length() > 0.01:
+			look_at(global_position + horizontal_velocity, Vector3.UP)
+		move_and_slide()
 
 func loseAttention():
 	target = possies.get_random_pos()
--- World/The world/main.gd
+++ World/The world/main.gd
@@ -2,5 +2,18 @@ extends Node3D
 
 @export var possies: Node3D
 
+@export var bin: Node3D
+
+@export var game_started: bool = false
+
+@export var brammy: enemy
+
 func _ready() -> void:
 	GlobalRefs.world_positions = possies
+
+
+func _on_start_sequence_detection_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player") and !game_started:
+		game_started = true
+		bin.ap.play("bram_pop_out")
+		brammy.started = true
