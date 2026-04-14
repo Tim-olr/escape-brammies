1b0e1d1ec82f5365036bc873671c3e7f7e648505
Tim-olr <timolree1@gmail.com>
2026-04-14 17:12:59 +0200

    hollup, added lerp

--- World/The world/main.gd
+++ World/The world/main.gd
@@ -1,21 +1,30 @@
 extends Node3D
-
 @export var possies: Node3D
 @export var bin: Bin
 @export var game_started: bool = false
 @export var brammy: enemy
+var has_walked: bool = false
+var fov_tween: Tween
 
 func _ready() -> void:
 	GlobalRefs.world_positions = possies
+
+func lerp_fov(target: float, duration: float = 0.6) -> void:
+	if fov_tween:
+		fov_tween.kill()
+	fov_tween = create_tween()
+	fov_tween.tween_property(GlobalPlayer.camera, "fov", target, duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
 func _on_start_sequence_detection_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player") and !game_started:
 		game_started = true
 		bin.ap.play("bram_pop_out")
 		brammy.started = true
-		GlobalPlayer.camera.fov = 32.0
+		lerp_fov(32.0)
+		GlobalPlayer.promptinstance.show_prompt("BRAMMY HAS AWOKEN")
 
 func _on_start_sequence_detection_body_exited(body: Node3D) -> void:
-	if body.is_in_group("player") and game_started:
-		GlobalPlayer.camera.fov = 75.0
+	if body.is_in_group("player") and game_started and !has_walked:
+		lerp_fov(75.0)
 		bin.ap.play("bram_in")
+		has_walked = true
