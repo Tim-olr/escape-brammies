8419ac07c6caef0db34af8e1a6811a5cfa0b44fa
Tim-olr <timolree1@gmail.com>
2026-04-15 12:55:39 +0200

    uh oh..

--- World/The world/main.gd
+++ World/The world/main.gd
@@ -28,6 +28,7 @@ func _on_start_sequence_detection_body_entered(body: Node3D) -> void:
 		game_started = true
 		bin.ap.play("bram_pop_out")
 		bin.audio.play()
+		GlobalRefs.brammy.footsteps.play()
 		brammy.started = true
 		var rand_pos = brammy_spawn_possies.get_children().pick_random()
 		brammy.global_position = rand_pos.global_position
