d7b55e72cbdf0fbd73f6516834742a94ab6ee922
Tim-olr <timolree1@gmail.com>
2026-04-15 18:25:05 +0200

    fixed scrummas bellas beingas biggas

--- Items/interactables/scrumbel/scrumbel_interactable.gd
+++ Items/interactables/scrumbel/scrumbel_interactable.gd
@@ -4,6 +4,9 @@ const SCRUMBEL = preload("uid://sk8gw3cysmfg")
 
 var usable
 
+func _ready() -> void:
+	scale = Vector3(0.3, 0.3, 0.3)
+
 func interact() -> bool:
 	usable = SCRUMBEL.instantiate()
 	if GlobalPlayer.inventory.add_item(usable):
