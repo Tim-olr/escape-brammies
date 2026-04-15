eefac5a7e1e349d24a0417fe1cfe0e98c678c379
nullpointer <null.ptr.exc@outlook.com>
2026-04-15 10:04:45 +0200

    Meow

--- Brammy/audio_stream_player_3d.gd
+++ Brammy/audio_stream_player_3d.gd
@@ -1,3 +1,19 @@
 extends AudioStreamPlayer3D
 
 @export var audios: Array[AudioStream]
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	audio_proc()
+
+func choose_random_audio():
+	return audios.pick_random()
+
+func audio_proc():
+	var rand_sec = randf_range(15, 40)
+	timer.start(rand_sec)
+
+func _on_timer_timeout() -> void:
+	stream = choose_random_audio()
+	play()
+	audio_proc()
